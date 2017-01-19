4311e22148799c4ba4b90cffdd124b5d3fb0cae3
Alex Bell <bellifno@gmail.com>
2017-01-19 16:50:55 +0200

    delete all coments

--- login.php
+++ login.php
@@ -5,15 +5,11 @@
 </head>
 <body>
 	<?php
-	// session_start();
 		if (isset($_POST['log_in']) && !empty($_POST['login']) && !empty($_POST['password'])) {
 			if ($_POST['login'] == "admin" && $_POST['password'] == "admin123") {
-				// $_SESSION['valid'] = true;
-				// $_SESSION['login'] = "admin";
 
 				header("location: index.php");
 			} else {
-				// $error = "wroasng login or password";
 				echo '<script languge="javascript">';
 				echo 'alert("wrong login or password")';
 				echo '</script>';
